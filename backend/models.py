f9a2cb941fbfd5f08b6dc5f7b9f4ac13e9c93c0e
GreenCycle Developer <user@example.com>
2026-02-08 16:46:44 +0600

    bug fix

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 db = SQLAlchemy()
 
-class Admin(UserMixin, db.Model):
+class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
